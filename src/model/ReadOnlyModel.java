df5d1d854454640734316d0f2b420507377955d9
ART <armanruzgar@gmail.com>
2020-04-07 20:00:40 +0300

    java style kinda fixed

--- src/model/ReadOnlyModel.java
+++ src/model/ReadOnlyModel.java
@@ -2,12 +2,20 @@ package model;
 
 import java.util.Map;
 
+/**
+ * Class that represent Read Only Model.
+ * It is used to retrieve information from the model.
+ * Any methods that modify that modify the model throws UnsupportedOperationException.
+ */
 public class ReadOnlyModel implements Animator {
-
   private Animator readOnly;
 
+  /**
+   * Constructor for ReadOnlyModel to be read by the views.
+   *
+   * @param readOnly the BasicAnimator model used to retrieve information.
+   */
   public ReadOnlyModel(Animator readOnly) {
-
     this.readOnly = readOnly;
   }
 
